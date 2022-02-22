4760e3c0a02b624267814e291faff2499817a6d7
veshrao <veshhrao@gmail.com>
2022-02-22 22:45:36 +0800

    Revert "Fix net traffic view being white on QQS Header"
    This reverts commit 39d576ef0bf4a24dd3313eb8e564e3ffb081131e.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
@@ -315,6 +315,7 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
             filter.addAction(Intent.ACTION_SCREEN_ON);
             mContext.registerReceiver(mIntentReceiver, filter, null, getHandler());
         }
+        Dependency.get(DarkIconDispatcher.class).addDarkReceiver(this);
         updateSettings();
     }
 
@@ -325,6 +326,7 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
             mContext.unregisterReceiver(mIntentReceiver);
             mAttached = false;
         }
+        Dependency.get(DarkIconDispatcher.class).removeDarkReceiver(this);
     }
 
     private final BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
